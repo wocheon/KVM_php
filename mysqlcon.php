b78eb0fb98f355ae51fccce232e0273a5ec03a52
wocheon <82438284+wocheon@users.noreply.github.com>
2021-04-21 17:27:59 +0900

    Update mysqlcon.php

--- mysqlcon.php
+++ mysqlcon.php
@@ -1,16 +1,8 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	<title> 갤러리 </title>
+	<title> mysqlcontest </title>
 	<link rel="stylesheet" href="css1.css" type="text/css">
-	<style>
-	img {
-		witdh: 250px;
-		height: 154px;
-		float : left;
-		margin : 5px;
-		}
-	</style>
 	</head>
 
 	<body>
@@ -26,12 +18,12 @@
 	$con=mysql_connect($db_host,$db_user,$db_password,$db_name);
 	
 	if( mysql_connect_error($con) ) {
-		echo "MySQL 접속 실패 !!", "<br>";
-		echo "오류 원인 : ", mysql_connect_error();
+		echo "MySQL con FAIL !!", "<br>";
+		echo "ERROR : ", mysql_connect_error();
 		exit();
 		}
 	
-	echo "MYSQL 접속 성공";
+	echo "MYSQL con success!!";
 	mysql_close($con);
 ?>
 	
