d2b2013cfdc5e864b5a8b9a80ea2ee77d8f4ca72
wocheon <82438284+wocheon@users.noreply.github.com>
2021-04-25 06:45:51 +0900

    Update login.php

--- login.php
+++ login.php
@@ -6,26 +6,27 @@
 
         <body>
 <?php
-
+        session_destroy();
         $input_id =$_POST['input1'];
         $input_password =$_POST['input2'];
 
-        $con = mysqli_connect("192.168.1.101", "user1", "user1","testdb") or die ("Mysql connection fail !!!");
+        $con = mysqli_connect("172.17.02", "root", "test123","testdb") or die ("Mysql connection fail !!!");
         $sql = "SELECT password from usertbl where id ='$input_id'";
         $ret = mysqli_query($con, $sql);
         $row = mysqli_fetch_array($ret);
         $s=$row['password'];
 
       if (password_verify($input_password, $s)) {
+        session_start();
+        $_SESSION['id'] = $input_id;
         echo("<script>location.href='index1.php';</script>");
-        } 
+        }
       else {
         echo("<script>location.href='index.html';</script>");
         }
 
         mysqli_close($con);
 ?>
-
         </body>
 
 </HTML>
